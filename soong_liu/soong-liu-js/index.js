256bb6f2acb3c0789fb471623a71821b1db4bc05
audbongy <audrea@Audys-MBP.home>
2015-07-14 00:42:27 -0400

    testing putting dads stuff as <li> in about

---
+++ soong_liu/soong-liu-js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+
+$('#contact-link').click(function() {
+	$('#contact-info').addClass("animated bounceInLeft");
+	$('#contact-info').css("visibility", "visible");
+
+});
+
+
+});
+
+//<div id="contact-info" class="animated bounceInLeft"><p>' +305 Broadway Suite #802, New York NY 10007+ '</p><p>' +phone: 212.608.3700+'</p><p>'+email: info@soongliu.nyc+'</p></div>');
+
